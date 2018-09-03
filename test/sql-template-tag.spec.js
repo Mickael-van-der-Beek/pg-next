1319473bdc9ca052bc807e1347b7d3061ab3fee9
Mickael van der Beek <mickael.van.der.beek+github@gmail.com>
2018-09-03 18:41:21 +0200

    Fix bug around undefined values

--- test/sql-template-tag.spec.js
+++ test/sql-template-tag.spec.js
@@ -6,21 +6,26 @@ const { sqlEscapeIdentifier, sqlEscapeLiteral } = require('../lib/sql-escapes');
 
 describe('SQL template tag', () => {
   it('collects values and write placeholders', () => {
-    var literal = sql`foo ${123} ${'abc'} bar ${null}`;
+    const literal = sql`foo ${123} ${'abc'} bar ${null}`;
     assert.equal(literal.text, 'foo $1 $2 bar $3');
     assert.deepEqual(literal.values, [123, 'abc', null]);
   });
   it('accepts nested literals', () => {
-    var literal = sql`foo ${sql`bar ${1} ${2} ${sql`baz`}`} ${sql`foobar ${3}`} ${4}`;
+    const literal = sql`foo ${sql`bar ${1} ${2} ${sql`baz`}`} ${sql`foobar ${3}`} ${4}`;
     assert.equal(literal.text, 'foo bar $1 $2 baz foobar $3 $4');
     assert.deepEqual(literal.values, [1, 2, 3, 4]);
   });
+  it('accepts nested literals with undefined and null values', () => {
+    const literal = sql`foo ${sql`bar ${null} ${2} ${sql`baz`}`} ${sql`foobar ${undefined}`} ${4}`;
+    assert.equal(literal.text, 'foo bar $1 $2 baz foobar $3 $4');
+    assert.deepEqual(literal.values, [null, 2, undefined, 4]);
+  });
   it('does not interpolate arrays into values array', () => {
-    var literal = sql`${[1, 2, 3]}`;
+    const literal = sql`${[1, 2, 3]}`;
     assert.deepEqual(literal.values, [[1, 2, 3]]);
   });
   it('does not interpolate escaped identifiers or literals', () => {
-    var literal = sql`foo ${sqlEscapeIdentifier('bar')} ${sqlEscapeLiteral('b\\az')} bat`;
+    const literal = sql`foo ${sqlEscapeIdentifier('bar')} ${sqlEscapeLiteral('b\\az')} bat`;
     assert.equal(literal.text, 'foo "bar"  E\'b\\\\az\' bat');
     assert.deepEqual(literal.values, []);
   });
